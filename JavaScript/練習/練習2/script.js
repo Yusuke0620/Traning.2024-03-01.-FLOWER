7dfb41b2c8f2c60e3b85734dd60fd29d431c0c03
Yusuke0620 <boston19851229@gmail.com>
2024-02-22 20:47:58 +0900

    変更の内容を簡潔に説明するメッセージ

--- JavaScript/練習/練習2/script.js
+++
@@ -1,8 +0,0 @@
-// カラーピッカーを選択したら、背景が同じ色になる
-
-const text = document.querySelector("#text");
-const color = document.querySelector("#count");
-
-color.addEventListener("load", () => {
-    document.body.value;
-});
