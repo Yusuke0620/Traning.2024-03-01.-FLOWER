376c9725cda535bf9fcb4ddf001732c082e0ab95
Yusuke0620 <boston19851229@gmail.com>
2024-03-07 02:59:53 +0900

    Completed coding for today

--- HTML - CSS/3月模写/1_FLOWER/script.js
+++ HTML - CSS/3月模写/1_FLOWER/script.js
@@ -1,12 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {  
+
+/*-------------------------------------------
+メインロゴホバーアニメーション
+-------------------------------------------*/
+  // ウェブページが開かれたとき
+  const fadeIn = document.querySelector('.mainLogo'); //メインロゴ格納  
+
+  // アニメーションのキーフレームを定義
+  const keyframes = {
+    opacity: [0, 1],  //透明から不透明
+    translate: ['0 5px', '0 0']
+  };
+
+  // アニメーションのオプション定義。時間とイージング関数
+  const options = {
+    duration: 2250,
+    easing: 'ease-in-out',
+  };
+
+  // 定義されたアニメーションを実施
+  fadeIn.animate(keyframes, options);
+
+  setTimeout(() => {
+    console.log('メインロゴが浮かび上がりました'); //ログを出力 
+  }, 2000);  
+
+/*-------------------------------------------
+レスポンシブ・スクロールフッター出現
+-------------------------------------------*/
+// 垂直方向のスクロール量を取得
+const scrollY = window.scrollY;
+console.log(scrollY); //ログを出力 
+
+// 横幅が531px以下 & 1645スクロールされた位置でフッター表示
+// 上から1644スクロールでフッター非表示
+
+
+
+
+/*-------------------------------------------
+フォームボタンを開く
+-------------------------------------------*/
   const supportButton = document.getElementById('supportButton'); //サポートボタン格納
   const formContainer = document.getElementById('formContainer'); //フォームコンテナ格納 
   const closeFormButton = document.getElementById('closeFormButton'); //クローズボタン格納
-});
 
-/* フォームボタンを開く */
-
-  // サポートボタンクリックイベント
+// サポートボタンクリックイベント
   supportButton.addEventListener('click', () => {    
     console.log('サポートボタンがクリックされました'); //ログを出力 
         
@@ -16,46 +55,48 @@ document.addEventListener('DOMContentLoaded', () => {
     supportButton.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
     setTimeout(() => {
       console.log('ボタンが非表示になりました'); //ログを出力
-    }, '1500'); 
+    }, 500); 
 
+    setTimeout(() => {
     // フォームコンテナを1秒後に表示
     formContainer.style.display = 'block';
-    formContainer.style.transition = 'opacity 1s ease'
-    console.log('フォームが表示されました'); //ログを出力  
-    });
+    console.log('フォームが表示されました'); //ログを出力
+    }, 700);    
+});
 
-
-/* フォームボタンを閉じる */
+/*-------------------------------------------
+フォームボタンを閉じる
+-------------------------------------------*/
 
   // クローズボタンクリックイベント
   closeFormButton.addEventListener('click', () => {
     console.log('クローズボタンがクリックされました'); //ログを出力 
-    
+    //フォーム非表示
     formContainer.style.display = 'none';
     console.log('フォームが非表示になりました'); //ログを出力
-
     //0.5秒後に1秒かけて再表示スタート
     setTimeout(() => {
       supportButton.style.opacity = '1';
       supportButton.style.transform = 'translateY(0)'; //元の位置で再表示される
       supportButton.style.transition = 'opacity 0.5s ease';
       console.log('サポートボタンが再表示されました'); //ログを出力
-    }, '500');
-
-
+    }, 500);
 });
 
-/* 添付ファイルhover発火イベント */
+/*-------------------------------------------
+添付ファイルhover発火イベント
+-------------------------------------------*/
 
-const fileUpload = document.querySelector('#fileUpload h4');
-const fileLabel = document.querySelector('.file-label');
-//ドキュメントにマウスが乗るとhover発火
-fileUpload.addEventListener('mouseover', () => {
-  fileLabel.classList.add('hover');
-  console.log('添付ファイル追加ドキュメントにマウスが乗りました');
-});
-//ドキュメントからマウスが離れるとhover終了
-fileUpload.addEventListener('mouseout', () => {
-  fileLabel.classList.remove('hover');
-  console.log('マウスが離れました');
+  const fileUpload = document.querySelector('#fileUpload h4');
+  const fileLabel = document.querySelector('.file-label');
+  //ドキュメントにマウスが乗るとhover発火
+  fileUpload.addEventListener('mouseover', () => {
+    fileLabel.classList.add('hover');
+    console.log('添付ファイル追加ドキュメントにマウスが乗りました');
+  });
+  //ドキュメントからマウスが離れるとhover終了
+  fileUpload.addEventListener('mouseout', () => {
+    fileLabel.classList.remove('hover');
+    console.log('マウスが離れました');
+  });
 });
